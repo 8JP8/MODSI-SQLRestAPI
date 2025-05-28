1c23ad346805de286b9d9c921f39b1c24c70dc8e
João Oliveira <1240369@isep.ipp.pt>
2025-05-28 18:33:31 +0100

    change of the endpoints
    changed email messages

--- User/Email/EmailTemplates.cs
+++ User/Email/EmailTemplates.cs
@@ -5,24 +5,21 @@ namespace MODSI_SQLRestAPI.UserAuth.Email
         public static string VerificationEmail(string link, string code)
         {
             return $@"
-                <p>Olá,</p>
                 <p>Clique no link para verificar a sua conta:</p>
-                <a href=""{link}"">{link}</a>
-                <br><br>
                 Código: {code}
                 <br><br>
-                Se não pediu esta verificação, ignore este email.
+                <a href=""{link}"">{link}</a>
+                <br><br>
             ";
         }
 
         public static string PasswordResetEmail(string link, string code)
         {
             return $@"
-                <p>Olá,</p>
                 <p>Clique no link para resetar a sua senha:</p>
-                <a href=""{link}"">{link}</a>
-                <br><br>
                 Código: {code}
+                <br><br>
+                <a href=""{link}"">{link}</a>
                 <br><br>
                 Se não pediu esta alteração, ignore este email.
             ";
